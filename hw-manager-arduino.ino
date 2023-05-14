7d25ffc710430213aa8ae4a97a02a217cce402dd
KirillKrasnikov <kirill.mdr@gmailc.om>
2023-05-14 04:24:13 +0300

    v2

--- hw-manager-arduino.ino
+++ hw-manager-arduino.ino
@@ -1,9 +1,19 @@
 // Configuration
+//PCdata[0] - is const speed
+//PCdata[1] - const speed
+//PCdata[2] - min temperature limit
+//PCdata[3] - max temperature limit
+//PCdata[4] - min speed limit
+//PCdata[5] - max speed limit
+//PCdata[6] - is dynamic light
+//PCdata[7] - brightness
+//PCdata[8] - red
+//PCdata[9] - green
+//PCdata[10] - blue
 byte speedMIN = 5;
 byte speedMAX = 95;
 byte tempMIN = 23;
 byte tempMAX = 40;
-
 
 // Pins
 #define FAN_PIN 9      // fans pin
@@ -31,7 +41,6 @@ unsigned long timeout;
 boolean updateTempFlag = 1;
 int duty, LEDcolor;
 int brightness, k, R, G, B, Rf, Gf, Bf;
-byte mainTemp;
 
 byte temp1;
 
@@ -76,23 +85,34 @@ void getTemperature() {
   }
 }
 void LEDcontrol() {
-  brightness = 100;
-  LEDcolor = map(mainTemp, tempMIN, tempMAX, 0, 1000);
-  LEDcolor = constrain(LEDcolor, 0, 1000);
-
-  if (LEDcolor <= 500) {
-    k = map(LEDcolor, 0, 500, 0, 255);
-    R = 0;
-    G = k;
-    B = 255 - k;
-  }
-  if (LEDcolor > 500) {
-    k = map(LEDcolor, 500, 1000, 0, 255);
-    R = k;
-    G = 255 - k;
-    B = 0;
+  if (hasSettingsFlag) {
+    brightness = PCdata[7];
+    LEDcolor = map(temp1, PCdata[2], PCdata[3], 0, 1000);
+  } else {
+    brightness = 100;
+    LEDcolor = map(temp1, tempMIN, tempMAX, 0, 1000);
   }
 
+  if (hasSettingsFlag && PCdata[6] == 0) {
+    R = PCdata[8];
+    G = PCdata[9];
+    B = PCdata[10];
+  } else {
+    LEDcolor = constrain(LEDcolor, 0, 1000);
+
+    if (LEDcolor <= 500) {
+      k = map(LEDcolor, 0, 500, 0, 255);
+      R = 0;
+      G = k;
+      B = 255 - k;
+    }
+    if (LEDcolor > 500) {
+      k = map(LEDcolor, 500, 1000, 0, 255);
+      R = k;
+      G = 255 - k;
+      B = 0;
+    }
+  }
 
   Rf = (brightness * R / 100);
   Gf = (brightness * G / 100);
@@ -104,21 +124,29 @@ void LEDcontrol() {
 }
 
 void dutyCalculate() {
-  if (temp1 < tempMIN) {
-    duty = 0;
+  if (hasSettingsFlag) {
+    if (PCdata[0] == 1) {
+      duty = PCdata[1];
+    } else {
+      duty = map(temp1, PCdata[2], PCdata[3], PCdata[4], PCdata[5]);
+    }
   } else {
     duty = map(temp1, tempMIN, tempMAX, speedMIN, speedMAX);
+  }
+
+  if (duty < 5) {
+    duty = 5;
   }
 }
 
 void timeoutTick() {
   if (millis() - timeout > 500) {
     updateTempFlag = 1;
-    // getTemperature();
-    // Serial.print("temp = ");
-    // Serial.println(temp1);
-    // Serial.print("duty = ");
-    // Serial.println(duty);
+    getTemperature();
+    Serial.print("temp = ");
+    Serial.println(temp1);
+    Serial.print("duty = ");
+    Serial.println(duty);
     // Serial.print("led = ");
     // Serial.println(LEDcolor);
     timeout = millis();
